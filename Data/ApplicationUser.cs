926e0f0cd562d291f8d3483793720641e4b7123d
lilskia1 <iminepotato@gmail.com>
2025-08-27 21:39:35 -0400

    Agregada carpeta de pruebas y cambios en ApplicationUser

--- Data/ApplicationUser.cs
+++ Data/ApplicationUser.cs
@@ -1,9 +1,12 @@
+using System.Diagnostics;
 using Microsoft.AspNetCore.Identity;
 
 namespace VaxSync.Web.Data;
 
 // Add profile data for application users by adding properties to the ApplicationUser class
+[DebuggerDisplay($"{{{nameof(DebuggerDisplay)}(),nq}}")]
 public class ApplicationUser : IdentityUser
 {
+    private string DebuggerDisplay => ToString();
 }
 
