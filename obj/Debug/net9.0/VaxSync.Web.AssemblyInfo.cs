7d2c9dbe20ea2a3720b96b748fdc6bb2f1253509
d4nnywoods <dmt_31@live.com>
2025-10-18 23:02:59 -0400

    a
    a

--- obj/Debug/net9.0/VaxSync.Web.AssemblyInfo.cs
+++ obj/Debug/net9.0/VaxSync.Web.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("VaxSync.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+78741b4c44bb4aa14a343d5ea6d8f7c6f851730a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dc83bff5116c7091157458557dfa3f691e8607a7")]
 [assembly: System.Reflection.AssemblyProductAttribute("VaxSync.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("VaxSync.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
